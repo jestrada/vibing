98ad5aa17e92c3f7b3e6825d5ff61a312e6bd0d6
Joseph Estrada <j.r.estrada@gmail.com>
2025-03-21 08:33:13 -0700

    add egg-prices.

---
+++ egg-prices/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to My React App</h1>
+      <p>Start editing to build your application!</p>
+    </div>
+  );
+}
+
+export default App;
